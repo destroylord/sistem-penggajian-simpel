26e616fc2a63d184dc6b35bda9ffc1cdac8b6276
destroylord <masapin68@gmail.com>
2022-01-08 10:12:33 +0700

    Add: add migratrion employee & salary employee

---
+++ database/migrations/2022_01_08_025029_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Facade\Ignition\Tabs\Tab;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name_employee');
+            $table->smallInteger('total_salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
---
+++ database/migrations/2022_01_08_025252_create_employee_salaries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_salaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('id_employee');
+            $table->timestamps();
+
+            $table->foreign('id')
+                    ->refrences('id_employee')
+                    ->on('employee');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_salaries');
+    }
+}
